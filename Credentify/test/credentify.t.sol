d673555e7d1cecc473859c24cf22579be93534bd
Aditya Ingole <aditayingole113@gmail.com>
2025-04-21 21:46:57 +0530

    initial commit before installing dependencies

--- Credentify/test/credentify.t.sol
+++ Credentify/test/credentify.t.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {credentify} from "../src/credentify.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract credentify is Test {
+    credentify public counter;
 
     function setUp() public {
-        counter = new Counter();
+        counter = new credentify();
         counter.setNumber(0);
     }
 
